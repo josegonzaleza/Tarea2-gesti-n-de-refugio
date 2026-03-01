eb0aad885c59ecc52978013a20d2f8d83e0c29b0
josegonzaleza <josegonzaleza@uned.cr>
2026-03-01 03:14:33 -0600

    Creacion y codificacion de DashboardController

---
+++ Tarea2/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Tarea2.Filters;
+using Tarea2.Models;
+
+namespace Tarea2.Controllers
+{
+    [RequireAuth]
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            var s = Session["user"] as UserSession;
+            ViewBag.Role = s.Role;
+            ViewBag.Email = s.Email;
+            return View();
+        }
+    }
+}
