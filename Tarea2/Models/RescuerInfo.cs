ca0dcd8d726b0c5efcd0ec8dd8b28f45d7419f78
josegonzaleza <josegonzaleza@uned.cr>
2026-02-28 23:35:45 -0600

    Creacion del modelo del rescatista

---
+++ Tarea2/Models/RescuerInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Tarea2.Models
+{
+    public class RescuerInfo
+    {
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
