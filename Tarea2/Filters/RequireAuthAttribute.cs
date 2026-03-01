bd71dac7c22a814c06c94c32c06c8e1843677038
josegonzaleza <josegonzaleza@uned.cr>
2026-02-28 23:51:37 -0600

    Creacion de filtro de autorizacion para logueos

---
+++ Tarea2/Filters/RequireAuthAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Tarea2.Models;
+
+namespace Tarea2.Filters
+{
+    public class RequireAuthAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var user = filterContext.HttpContext.Session["user"] as UserSession;
+            if (user == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new System.Web.Routing.RouteValueDictionary(
+                        new { controller = "Auth", action = "Login" }
+                    )
+                );
+                return;
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
