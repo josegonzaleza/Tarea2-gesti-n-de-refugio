bc08a65c29c94099185336bef578f4e1e8136319
josegonzaleza <josegonzaleza@uned.cr>
2026-02-28 23:37:34 -0600

    Creacion del modelo de tratamiento

---
+++ Tarea2/Models/Treatment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tarea2.Models
+{
+    public class Treatment
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }         
+        public string Type { get; set; }           
+        public string MedicineId { get; set; }     
+        public string MedicineName { get; set; }
+        public string Dose { get; set; }           
+        public string Observations { get; set; }   
+        public string CaregiverEmail { get; set; } 
+    }
+}
