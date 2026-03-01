f78d024e15bfa180370ee5a2bae2a3f36ac7821c
josegonzaleza <josegonzaleza@uned.cr>
2026-02-28 23:30:13 -0600

    Creacion y configuracion del modelo temporal de animales

---
+++ Tarea2/Models/AnimalDraft.cs
@@ -0,0 +1,24 @@
+﻿namespace Tarea2.Models
+{
+    public class AnimalDraft
+    {
+  
+        public string Nombre { get; set; }      
+        public string Especie { get; set; }     
+        public string Raza { get; set; }        
+        public string EdadAprox { get; set; }   
+        public string Sexo { get; set; }       
+        public string Color { get; set; }      
+
+        public string MotivoIngreso { get; set; }           
+        public string Temperamento { get; set; }            
+        public string NecesidadesMedicas { get; set; }      
+        public string HistorialVacunas { get; set; }        
+        public string EstadoSaludIngreso { get; set; }     
+
+    
+        public string RescatistaNombre { get; set; }       
+        public string RescatistaTelefono { get; set; }      
+        public string RescatistaCorreo { get; set; }       
+    }
+}
